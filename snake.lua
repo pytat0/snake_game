19bd8df70ebbe7c50709c01707924bfc61b777b6
Victor <vitygromyko@gmail.com>
2025-03-22 01:09:21 +0400

    replace body to linked list

--- snake.lua
+++ snake.lua
@@ -1,34 +1,28 @@
 local config = require 'config'
 local draw = require 'draw'
+local LinkedList = require 'linked_list'
 
 --- @class Snake
 local Snake = {
     head_x = 6,
     head_y = 6,
-    body = {
-        { 6, 7 },
-        { 7, 7 },
-    },
+    ---@type LinkedList
+    body = LinkedList:new(),
     should_grow = 0,
 }
+Snake.body:add_last({ 6, 7 })
+Snake.body:add_last({ 7, 7 })
 
 function Snake:move(dx, dy)
     local new_x = self.head_x + dx
     local new_y = self.head_y + dy
-    if
-        new_x >= 1
-        and new_x <= config.field.tile_count_x
-        and new_y >= 1
-        and new_y <= config.field.tile_count_y
-    then
-        local last = table.remove(self.body, #self.body)
-        table.insert(self.body, 1, { self.head_x, self.head_y })
-        self.head_x = self.head_x + dx
-        self.head_y = self.head_y + dy
-        if self.should_grow > 0 then
-            table.insert(self.body, #self.body + 1, last)
-            self.should_grow = self.should_grow - 1
-        end
+    local last = self.body:pop_last()
+    self.body:add_first({ self.head_x, self.head_y })
+    self.head_x = self.head_x + dx
+    self.head_y = self.head_y + dy
+    if self.should_grow > 0 then
+        self.body:add_last(last)
+        self.should_grow = self.should_grow - 1
     end
 end
 
@@ -37,8 +31,8 @@ function Snake:increase_size(count)
 end
 
 function Snake:draw()
-    for i, v in ipairs(self.body) do
-        draw.draw_box(v[1], v[2], config.colors.yellow)
+    for v in self.body:iter() do
+        draw.draw_box(v.value[1], v.value[2], config.colors.yellow)
     end
     draw.draw_box(self.head_x, self.head_y, config.colors.green)
 end
